4411cb986cb8cb764a68ba6b588d266813716df4
SeguridadC4E <seguridadc4e@gmail.com>
2023-09-30 09:57:05 -0500

    se agrega la portada

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const Main = () => {
   return (
     <main className='main'>
@@ -6,6 +8,7 @@ const Main = () => {
           <h2 className='front-div-h2'>seguridad</h2>
           <h2 className='front-div-h2 front-div-h2-space'>privada</h2>
         </div>
+        <Link to='/services' className='front-button'>mas informacion</Link>
       </section>
     </main>
   )
