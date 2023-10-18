4006738b5507b60a6c84db699012100ce1a85cee
SeguridadC4E <seguridadc4e@gmail.com>
2023-10-18 10:37:49 -0500

    se corrige el problema de la barra horizontal en laparte inferior de la ventana

--- src/fragments/Logo.jsx
+++ src/fragments/Logo.jsx
@@ -1,9 +1,11 @@
-import c4eLogo from './../assets/logo-c4e.png'
+import onlyLogoC4E from './../assets/only-logo-c4e.png'
+import fullLogoC4E from './../assets/full-name-logo-c4e.png'
 
 const Logo = () => {
   return (
     <div className='logo'>
-      <img src={c4eLogo} alt='logo de la empresa de seguridad C4E' className='logo-img' />
+      <img src={onlyLogoC4E} alt='logotipo de la empresa de seguridad C4E' className='logo-img' />
+      <img src={fullLogoC4E} alt='logotipo de la empresa de seguridad C4E' className='logo-img logo-img--fullName' />
     </div>
   )
 }
