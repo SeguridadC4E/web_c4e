a3513aaec2aec84e8e32198a28a6b1127d266f9e
HenryAzogue <hnrazogue@gmail.com>
2023-11-03 20:42:03 -0500

    se agrega una actualizacion de los clientes-cambio de formato

--- src/fragments/LogoScrolling.jsx
+++ src/fragments/LogoScrolling.jsx
@@ -3,20 +3,26 @@ import logoFarmaciasEconomicas from './../assets/image-partners/farmacias-econom
 import logoJena from './../assets/image-partners/jena.png'
 import logoMykRectificadora from './../assets/image-partners/myk-rectificadora.png'
 import logoUep from './../assets/image-partners/uep.jpg'
-import logoUep2 from './../assets/image-partners/uep.png'
 import logoWesternUnion from './../assets/image-partners/western-union.jpeg'
 
 const LogoScrolling = () => {
   return (
-    <div className='fragment-slider'>
-      <div className='fragment-slide-track'>
-        <div className='fragment-slide'><img src={logoCruzAzul} alt='Image 3' /></div>
-        <div className='fragment-slide'><img src={logoFarmaciasEconomicas} alt='Image 3' /></div>
-        <div className='fragment-slide'><img src={logoJena} alt='Image 3' /></div>
-        <div className='fragment-slide'><img src={logoMykRectificadora} alt='Image 3' /></div>
-        <div className='fragment-slide'><img src={logoUep} alt='Image 3' /></div>
-        <div className='fragment-slide'><img src={logoWesternUnion} alt='Image 3' /></div>
-        <div className='fragment-slide'><img src={logoUep2} alt='Image 3' /></div>
+    <div className='fragment-logos'>
+      <div className='fragment-logos-slide'>
+        <img src={logoJena} alt='Image 3' />
+        <img src={logoFarmaciasEconomicas} alt='Image 3' />
+        <img src={logoCruzAzul} alt='Image 3' />
+        <img src={logoWesternUnion} alt='Image 3' />
+        <img src={logoUep} alt='Image 3' />
+        <img src={logoMykRectificadora} alt='Image 3' />
+      </div>
+      <div className='fragment-logos-slide'>
+        <img src={logoJena} alt='Image 3' />
+        <img src={logoFarmaciasEconomicas} alt='Image 3' />
+        <img src={logoCruzAzul} alt='Image 3' />
+        <img src={logoWesternUnion} alt='Image 3' />
+        <img src={logoUep} alt='Image 3' />
+        <img src={logoMykRectificadora} alt='Image 3' />
       </div>
     </div>
   )
