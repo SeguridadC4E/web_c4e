d62a34da871fa0f00fe390a7f6101d0178b52584
HenryAzogue <hnrazogue@gmail.com>
2023-11-15 12:35:05 -0500

    trabajando el modo responsive en la pagina Nosotros

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,19 +1,22 @@
 import './About.css'
 import trustAbout from './../assets/img-content/trust-about.jpg'
-import LogoScrolling from '../fragments/LogoScrolling'
 
 const About = () => {
   return (
-    <main className='page-about'>
-      <header className='page-about-header'>
-        <div className='page-about-header-div'>
-          <h3 className='page-about-header-title'>seguridad</h3>
-          <h3 className='page-about-header-title'>protección</h3>
-          <h3 className='page-about-header-title'>tecnología en seguridad</h3>
+    <main className='about'>
+      <section className='section-heroe'>
+        <div className='heroe-big'>
+          <img
+            className='heroe-big-img'
+            src='https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+            alt='imagen de fondo'
+          />
         </div>
-        <p className='page-about-header-text'>Brindamos a nuestros clientes la tranquilidad necesaria para que puedan llevar a cabo sus actividades cotidianas con total confianza en su seguridad.</p>
-      </header>
-      <section className='page-about-section-intro'>
+        <div className='heroe-paragraph'>
+          <h1 className='heroe-paragraph-title'>El origen de nuesta historia</h1>
+        </div>
+      </section>
+      <section className='section-about-intro'>
         <h2 className='page-about-section-intro-h2'>acerca de nosotros</h2>
         <div className='page-about-section-intro-div'>
           <p className='page-about-section-intro-div-p'>Somos C4E Cuji Seguridad del Ecuador Cia. Ltda., una compañía de seguridad acreditada según la Resolución Ministerial Nro. MDI-VSC-SOP-DRMSP-SP. Esta acreditación es avalada por Pablo Francisco Coello Larco, el Director de Regulación y Monitoreo de Seguridad Privada. De acuerdo con el Acuerdo Ministerial Nro. 196, publicado en el Registro Oficial Nro. 148, se expide el Reglamento para Regular el Procedimiento Administrativo y Sancionatorio de las Compañías de Vigilancia y Seguridad Privada. Este reglamento establece el procedimiento administrativo para el otorgamiento del permiso de operación y funcionamiento de las compañías de vigilancia y seguridad privada.</p>
@@ -44,12 +47,6 @@ const About = () => {
           <p className='page-about-section-div-p'>Nuestra visión es crear un entorno donde la seguridad sea sinónimo de tranquilidad, confianza y calidad de vida. Nos esforzamos por ofrecer soluciones personalizadas y superar las expectativas de nuestros clientes. A medida que avanzamos, nuestra pasión por la seguridad sigue siendo el motor que nos impulsa a crecer y expandir nuestra huella, garantizando un lugar más seguro para todos.</p>
         </div>
         <img src='https://images.unsplash.com/photo-1542769494-0cc077eead4b?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1974' alt='image abot' className='page-about-section-img' />
-      </section>
-      <section className='page-about-section-partners'>
-        <h3 className='page-about-section-partners-h3'>nuestros clientes</h3>
-        <div className='page-about-section-div-partners'>
-          <LogoScrolling />
-        </div>
       </section>
     </main>
   )
