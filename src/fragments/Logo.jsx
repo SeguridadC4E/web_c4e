44485c417756f6e71432557e579dca2901fdb68a
HenryAzogue <hnrazogue@gmail.com>
2023-11-21 20:47:39 -0500

    se modifica el logo y cambio a la metodologia BEM para los estilos

--- src/fragments/Logo.jsx
+++ src/fragments/Logo.jsx
@@ -1,13 +1,17 @@
 import './Logo.css'
-import onlyLogoC4E from './../assets/only-logo-c4e.png'
-import fullLogoC4E from './../assets/full-name-logo-c4e.png'
 import { Link } from 'react-router-dom'
 
 export default function Logo () {
   return (
     <Link to='/' className='logo' target='_self' rel='nofollow noopener noreferrer'>
-      <img src={onlyLogoC4E} alt='logotipo de la empresa de seguridad C4E' className='logo-img' />
-      <img src={fullLogoC4E} alt='logotipo de la empresa de seguridad C4E' className='logo-img logo-img--fullName' />
+      <div className='logo__content-one'>
+        <h1 className='logo__title'><span>C</span><span className='logo__span-fourt'>4</span><span>E</span></h1>
+        <span className='logo__span'>\</span>
+      </div>
+      <div className='logo__content-two'>
+        <h2 className='logo__subtitle'>CUJI SEGURIDAD</h2>
+        <h2 className='logo__subtitle'> DEL ECUADOR CIA. LTDA.</h2>
+      </div>
     </Link>
   )
 }
